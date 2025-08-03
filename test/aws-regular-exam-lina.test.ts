9ef27f09352bd4dc57b8426dd3d1e856b2171b5e
Zlatelina Kazakova <lina@Zlatelinas-MacBook-Pro-2.local>
2025-08-03 11:41:02 +0300

    tests

--- test/aws-regular-exam-lina.test.ts
+++ test/aws-regular-exam-lina.test.ts
@@ -1,17 +1,11 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsRegularExamLina from '../lib/aws-regular-exam-lina-stack';
+import {Template} from "aws-cdk-lib/assertions";
+import {App} from "aws-cdk-lib";
+import { AwsRegularExamLinaStack } from "../lib/aws-regular-exam-lina-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-regular-exam-lina-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsRegularExamLina.AwsRegularExamLinaStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+  const app = new App();
+  const stack = new AwsRegularExamLinaStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  expect(template).toMatchSnapshot();
 });
